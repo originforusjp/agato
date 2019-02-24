81411af059016f518d76b657b026db236b209df7
甫足崇人 <pottary_a_akkun@ezweb.ne.jp>
2019-02-24 11:22:50 +0900

    Initial Commit

---
+++ Sample/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sample
+//
+//  Created by user on 2019/02/24.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sample
+//
+//  Created by user on 2019/02/24.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
